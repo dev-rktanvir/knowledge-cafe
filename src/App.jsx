b38a64ea1226f096fab31e7bcb90e76aa4787299
rk-tanvir790 <ktanvir790@gmail.com>
2025-08-07 01:18:51 +0600

    complete navbar & install react icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='w-full md:max-w-7xl mx-auto'>
+      <header>
+        <Navbar></Navbar>
+      </header>
+    </div>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import userPhoto from '../../assets/user.jpg';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-between px-4 sm:px-6 md:px-8 py-4 sm:py-6 md:py-8'>
+                {/* Left Side */}
+                <div>
+                    <h2 className='text-2xl sm:text-3xl md:text-4xl font-bold'>Knowledge Cafe</h2>
+                </div>
+                {/* Right Side */}
+                <div>
+                    <img
+                        className='w-10 h-10 sm:w-12 sm:h-12 md:w-[60px] md:h-[60px] rounded-full object-cover'
+                        src={userPhoto}
+                        alt="User"
+                    />
+                </div>
+            </div>
+            <div className='w-[95%] mx-auto'>
+                <hr />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
